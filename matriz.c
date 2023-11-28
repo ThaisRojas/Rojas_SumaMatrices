6db53fbce7639bc28c6f75e24e52676a330e4cab
Thais Rojas <thais.rojas@udla.edu.ec>
2023-11-28 15:23:07 -0500

    Llena las matrices

--- matriz.c
+++ matriz.c
@@ -10,3 +10,12 @@ void generarMatrizAleatoria(int filas, int columnas, int matriz[filas][columnas]
         }
     }
 }
+void imprimirMatriz(int filas, int columnas, int matriz[filas][columnas]) {
+    int i, j;
+    for (i = 0; i < filas; i++) {
+        for (j = 0; j < columnas; j++) {
+            printf("%d\t", matriz[i][j]); // LLena la matriz con numeros entre 0 y 100
+        }
+        printf("\n");
+    }
+}
